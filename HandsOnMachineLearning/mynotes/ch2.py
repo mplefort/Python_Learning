43d0b16f7c491cbc23cc3871d7d96f8579f4a63b
Matt LeFort <matthewlefort@gmail.com>
2019-06-26 12:26:16 -0700

    Completed up to pg 96. Select and Train a Model

--- HandsOnMachineLearning/mynotes/ch2.py
+++ HandsOnMachineLearning/mynotes/ch2.py
@@ -1,117 +1,225 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import scipy
-# import sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import	StratifiedShuffleSplit
-
-
+import sklearn
+import pandas as pd
 import os
-import sys
 
-# Set working directory to HandsOnMachineLearing
-os.chdir(os.path.join(os.getcwd(), 'HandsOnMachineLearning', 'mynotes') )
-# os.chdir("..")
-if os.path.basename(os.getcwd()) != "HandsOnMachineLearning":
-    print("Current directory not \'HandsOnMachineLearning\', at %s" % os.getcwd())
-    exit()
 HOUSING_PATH = os.path.join("datasets", "housing")
 
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
+# set pycharm to show each column when printing
+desired_width=320
+pd.set_option('display.width', desired_width)
+np.set_printoptions(linewidth=desired_width)
+pd.set_option('display.max_columns',10)
 
-
-def split_train_test(data: pd.DataFrame, test_ratio: float) -> object:
-    """
-        split data into test and train set. Typically 20% Test, 80% Train
-    :param data: data frame to split
-    :param test_ratio: float percent to keep as test ratio (0.0 - 1.0)
-    """
-
-    # create random indicies 0-len(data)
-    shuffled_indices = np.random.permutation(len(data))
-    test_set_size = int(len(data) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    return data.iloc[train_indices], data.iloc[test_indices]
-
-
-### Get the Data
-# Load the data
-housing = load_housing_data(HOUSING_PATH)
-
-## Explore the data - Notice tail heavy data, Caps in median income, home age, and home value, diff scales of values
+#  Get data
+housing = load_housing_data()
+## inspect data with:
+# housing.describe()
 # housing.hist(bins=50, figsize=(20,15))
-
-
-## Limit income range to categories and and reshape data to be more bell shaped pg. 76
-housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)  # limit income cats to 10 (15.0 / 1.5)
-housing["income_cat"].where(housing["income_cat"] < 5,	5.0,	inplace=True)  # rounds down cats > 5.0 (75K) to 75k
-# housing["income_cat"].hist()
 # plt.show()
 
-## Create a Test set
-# train_set, test_set = split_train_test(housing, 0.2)  # Using own split_train_test func
-train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)  # using scikit-learn
-print("%i train data + %i test data" % (len(train_set), len(test_set)) )
+"""
+Data splitting for test train set
+    - if offline ok to use a seeded random num gen to keep data in, if data updating will need to train new data in 
+    same order/split so a hash will need to be assigned to old and new data. 
 
-# with scikit-learn stratified split for equal weighting income cats
-split = StratifiedShuffleSplit(n_splits=1,	test_size=0.2,	random_state=42)
-for	train_index, test_index	in	split.split(housing, housing["income_cat"]):
-        strat_train_set = housing.loc[train_index]
-        strat_test_set = housing.loc[test_index]
+from sklearn.model_selection import train_test_split
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
+"""
 
-## Make a chart to show the comparison of stratified vs random samples on pg 78.
-# samplecomparedf = pd.DataFrame(
-#     data=(
-#             {'Overall': housing["income_cat"].value_counts() / len(housing),  # all data
-#              'Random': test_set["income_cat"].value_counts() / len(test_set),  # random samples
-#              'Strat': strat_test_set["income_cat"].value_counts() / len(strat_test_set)}  # strat samples
-#             )
-#     )
-#
-# samplecomparedf['Rand % err'] = np.abs(( samplecomparedf['Overall'] - samplecomparedf['Random'] ) / samplecomparedf['Overall']) * 100.0
-# samplecomparedf['Strat % err'] = np.abs(( samplecomparedf['Overall'] - samplecomparedf['Strat'] ) / samplecomparedf['Overall']) * 100.0
-#
-# # Shows strat error gets a more accurate distribution of income_cat in test/train split with close to
-# print(samplecomparedf.sort_index())
+"""
+Stratified sampling based off income
+    - more accurate test/train split data based off of distribution of data instead of just random samples
+"""
+from sklearn.model_selection import StratifiedShuffleSplit
 
-for	set_ in	(strat_train_set, strat_test_set):
-    set_.drop("income_cat",	axis=1,	inplace=True)  # remove income_cat as we only used it for stratified samples
+housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)  # scale data max 15 -> 10, 75% < 5, round up
+housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True) #cut off at 5.0
 
-### Discover and Visualize the Data to Gain Insights Pg. 80
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
 
-housing	= strat_train_set.copy() # create copy of training set to play/explore with
-## scatter plot of the data
-# housing.plot(kind="scatter", x="longitude",	y="latitude", alpha=0.4,
-#              s=housing["population"]/100, label="population", figsize=(10,7),  # radius is population size/100
-#              c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,  # color map is housing value
-#              )
+# drop "income_cat from test/train sets as stratified
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+"""
+Discover the data 
+    - remove test set of data to manipulate only training data. Even human can make overfitting occur if able to 
+    manipulate training set.
+    - using a scatter plot of map is a good option to see location to price and population values. 
+"""
+
+housing = strat_train_set.copy()
+# s = radius is population size,
+# c = color is housing price using jet scheme
+# housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+# s=housing["population"]/100, label="population", figsize=(10,7),
+# c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,)
 # plt.legend()
-# plt.show()
 
-## Looking for Correlations
-# from pandas.tools.plotting import scatter_matrix
-# corr_matrix = housing.corr()
-# print(corr_matrix['median_house_value'].sort_values(ascending=False))  # see that median income has a linear rel with house price
+"""
+Correlations 
+ - scatter plot hist of most correclated attributes to housing_value
+ - most promsing is median income with high corellation. Detailed scatter of it shows high freq of points at the 350,
+ 450, and 500 
+"""
+from pandas.plotting import scatter_matrix
 
-# attributes = ['median_house_value', 'median_income', 'total_rooms', 'housing_median_age']  # plot to make sure corr matrix zeros are actual no correlation
-# scatter_matrix(housing[attributes])
-# housing.plot(kind='scatter', x='median_income', y='median_house_value')
-# plt.show()
+print(housing.corr())
+attributes = ["median_house_value", "median_income", "total_rooms", "housing_median_age"]
+# scatter_matrix(housing[attributes], figsize=(12, 8))
+# housing.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
 
-## Experimenting with Attribute Combinations
-# housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
-# housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
-# housing["population_per_household"] = housing["population"]/housing["households"]
+"""
+Data attribute combinations
+"""
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"]=housing["population"]/housing["households"]
 
-# corr_matrix = housing.corr() # shows more correlation with "bedrooms_per_room" and "rooms_per_household"
-# print(corr_matrix['median_house_value'].sort_values(ascending=False))  # see that median income has a linear rel with house price
+corr_matrix = housing.corr()
+print(corr_matrix["median_house_value"].sort_values())
 
-### Prepare the Data for Machine Learning Algorithms
+""""
+---------------------- Prepare Data for Machine Learning pg. 87 ----------------------------------------------
+- transformation functions to prepare data. Start to build library of utils and prepare for future data updates as well.
+"""
+
+# return to original data set
+housing = strat_train_set.drop("median_house_value", axis=1)
+housing_labels = strat_train_set["median_house_value"].copy()
+
+"""
+Data cleaning
+ - remove or fill missing data
+"""
+from sklearn.preprocessing import Imputer
+
+imputer = Imputer(strategy='median')
+# remove non numerical values to calculate median
+housing_num = housing.drop("ocean_proximity",axis=1)
+imputer.fit(housing_num)
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns)
+
+""" 
+Handling Text Categories - with one_hot_encoding
+"""
+from sklearn.preprocessing import LabelBinarizer
+encoder = LabelBinarizer()
+housing_cat = housing["ocean_proximity"]
+housing_cat_1hot = encoder.fit_transform(housing_cat)
 
 
-print('done')
+""" 
+Custom Transformers for data cleaning
+ - Follow Scikit learns outline include a fit(), transform(), and fit_transform() method.
+    - makes easy to make training with and without certain transforms easy.
+"""
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
+
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+
+    def fit(self, X, y=None):
+        return self # nothing else to do
+
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+        population_per_household = X[:, population_ix] / X[:, household_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+# A numpy array of the extra attribs added using a transfromer function
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+
+
+"""
+Feature scaling
+ - There are two common ways to get all attributes to have the same scale: min-max scaling and standardization.
+    - min-max = scales to 0-1 by x = x[i] - x_min / (x_max - x_min). Subject to outliers ruining values. 
+        - MinMiaxScaler()
+    - standardization - normalize to guassian dist. Good for outlier datas, bad for ANNs as they expect 0-1
+        - StandardScaler()
+ - Always scale to training set and then apply to test.
+
+"""
+
+"""
+Transformation Pipelines
+    - SciKit pipeline for transforms
+    - 
+"""
+# convert the numpy.array from scikit-learns pipeline tranformers back into a pandas df
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.attribute_names].values
+
+
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+
+num_pipeline = Pipeline([ ('selector', DataFrameSelector(num_attribs)),
+                          ('imputer', Imputer(strategy="median")),
+                          ('attribs_adder', CombinedAttributesAdder()),
+                          ('std_scaler', StandardScaler()),
+                          ])
+
+cat_pipeline = Pipeline([('selector', DataFrameSelector(cat_attribs)),
+                         # ('label_binarizer', LabelBinarizer()),    # Cannot get to work, used a separate flow below to
+                         # complete issue.
+                         ])
+
+# combine the two pipelines
+from sklearn.pipeline import FeatureUnion
+full_pipeline = FeatureUnion(transformer_list=[
+    ("num_pipeline", num_pipeline),
+    ("cat_pipeline", cat_pipeline),
+    ])
+
+housing_prepared = full_pipeline.fit_transform(housing)
+
+# Sol for above error: https://stackoverflow.com/questions/46162855/fit-transform-takes-2-positional-arguments-but-3-were-given-with-labelbinarize
+housing_prepared = np.delete(housing_prepared,11,1)
+encoder = LabelBinarizer()
+housing_cat = housing["ocean_proximity"]
+housing_cat_1hot = encoder.fit_transform(housing_cat)
+
+housing_prepared = np.concatenate([housing_prepared, housing_cat_1hot], axis=1)
+
+
+"""
+------------------------  Select and Train a model --------------------------------
+pg. 96
+
+"""
