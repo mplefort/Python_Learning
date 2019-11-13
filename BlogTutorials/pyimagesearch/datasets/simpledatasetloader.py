95a8a5c6e5022fc713a653e84fcb9b97d79e78bd
Matthew <matthewlefort@gmail.com>
2019-11-13 12:03:07 -0800

    pick_confidence_net project added

--- BlogTutorials/pyimagesearch/datasets/simpledatasetloader.py
+++ BlogTutorials/pyimagesearch/datasets/simpledatasetloader.py
@@ -4,16 +4,17 @@
 import numpy as np
 import cv2
 import os
-
+import matplotlib.image as mpimg
 
 class SimpleDatasetLoader:
-    def __init__(self, preprocessors=None):
+    def __init__(self, preprocessors=None, img_load="cv"):
         '''
         Load data and store any preprocessors to be used on loaded image data
         :param preprocessors: preprocessors to be applied to loaded data to be applied sequentially in a pipeline
+        :param img_load: [str] use cv or matplotlib to load images. "cv" or "mat"
         '''
         self.preprocessors = preprocessors
-
+        self.img_load = img_load
         # if the preprocessors are None, initialize them as an empty list
         if self.preprocessors is None:
             self.preprocessors = []
@@ -33,7 +34,11 @@ class SimpleDatasetLoader:
         for (i, imagePath) in enumerate(imagePaths):
             # load the image and extract the class label assuming that our path has the following format:
             # /path/to/dataset/{class_label}/{image}.jpg
-            image = cv2.imread(imagePath)
+
+            if self.img_load == "cv":
+                image = cv2.imread(imagePath)
+            else:
+                image = mpimg.imread(imagePath)
             label = imagePath.split(os.path.sep)[-2]
 
             # check if need to apply a preprocessor
